413128b7f762599948558d1d0ad5c9c0006930f2
Kanika Banga <kanikabanga29@gmail.com>
2026-07-05 16:11:02 +0530

    Time: 7 ms (81.09%), Space: 19.6 MB (53.3%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i =0
+        j = len(s)-1
+        while j>i:
+            if s[i].lower() == s[j].lower():
+                i+=1
+                j-=1
+            elif s[i].isalnum() == False:
+                i+=1
+            elif s[j].isalnum() == False:
+                j-=1
+            else:
+                return False
+        return True
