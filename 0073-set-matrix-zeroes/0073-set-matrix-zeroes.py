d7992e7e66162691fe88671afeee96dc767fd99c
Kanika Banga <kanikabanga29@gmail.com>
2026-08-11 21:13:48 +0530

    Time: 7 ms (57.77%), Space: 20.8 MB (33.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,24 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def markInfinity(matrix,row,col):
-            r = len(matrix)
-            c = len(matrix[0])
-            for i in range(0,r):
-                if matrix[i][col]!=0:
-                    matrix[i][col] = float("inf")
-            for j in range(0,c):
-                if matrix[row][j]!=0:
-                    matrix[row][j] = float("inf")
-        
         r = len(matrix)
         c = len(matrix[0])
+        row_track = [0 for _ in range(0,r)]
+        col_track = [0 for _ in range(0,c)]
         for i in range(0,r):
             for j in range(0,c):
                 if matrix[i][j] == 0:
-                    markInfinity(matrix,i,j)
-
+                    row_track[i] = -1
+                    col_track[j] = -1
         for i in range(0,r):
             for j in range(0,c):
-                if matrix[i][j]==float("inf"):
+                if row_track[i]==-1 or col_track[j]==-1:
                     matrix[i][j] = 0
+                    
+        
