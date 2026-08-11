989bbeecdbc0d2377527b3a98b770375cad0d015
Kanika Banga <kanikabanga29@gmail.com>
2026-08-11 21:09:55 +0530

    Time: 322 ms (5.04%), Space: 21.4 MB (8.67%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,26 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        def markInfinity(matrix,row,col):
+            r = len(matrix)
+            c = len(matrix[0])
+            for i in range(0,r):
+                if matrix[i][col]!=0:
+                    matrix[i][col] = float("inf")
+            for j in range(0,c):
+                if matrix[row][j]!=0:
+                    matrix[row][j] = float("inf")
+        
+        r = len(matrix)
+        c = len(matrix[0])
+        for i in range(0,r):
+            for j in range(0,c):
+                if matrix[i][j] == 0:
+                    markInfinity(matrix,i,j)
+
+        for i in range(0,r):
+            for j in range(0,c):
+                if matrix[i][j]==float("inf"):
+                    matrix[i][j] = 0
